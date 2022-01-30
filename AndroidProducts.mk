9f4239d5564d2f00fe14fb7d4f7368cdd98ed0af
Jubayer Himel <64546040+jrhimel@users.noreply.github.com>
2022-01-30 16:32:15 +0000

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_TECNO-KF6k.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_TECNO-KF6k-user \
+    omni_TECNO-KF6k-userdebug \
+    omni_TECNO-KF6k-eng
